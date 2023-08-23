4f9d925cd4ebd0c9177cc7edda0c38fee3c2d512
Misho Dzuliashvili <87991798+mishodzuliashvili@users.noreply.github.com>
2023-08-23 21:46:23 +0400

    google-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,9 @@ import { compare } from "bcryptjs";
 import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
 function updateLastLoginTime(id: string) {
   return prisma.user.update({
     where: {
@@ -57,6 +59,10 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   pages: {
